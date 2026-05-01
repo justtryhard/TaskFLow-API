ca928f946f6bfcd6eec3c181fb9414b37a90fb05
justtryhard <lexchernobrov@gmail.com>
2026-05-01 20:24:23 +0300

    post login via redis

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,18 +1,44 @@
+import json
+
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models.user import User
-
+from app.core.redis import redis_client
 
 class UserRepository:
     def __init__(self, db: AsyncSession):
         self.db = db
 
     async def get_by_email(self, email: str):
+        cache_key = f"user:{email}"
+
+        # 1. пробуем из Redis
+        cached = await redis_client.get(cache_key)
+        if cached:
+            data = json.loads(cached)
+            return User(**data)
+
+        # 2. идём в БД
         result = await self.db.execute(
             select(User).where(User.email == email)
         )
-        return result.scalar_one_or_none()
+        user = result.scalar_one_or_none()
+
+        # 3. кладём в кэш
+        if user:
+            await redis_client.set(
+                cache_key,
+                json.dumps({
+                    "id": user.id,
+                    "email": user.email,
+                    "hashed_password": user.hashed_password,
+                    "is_active": user.is_active,
+                }),
+                ex=60,  # TTL 60 секунд
+            )
+
+        return user
 
     async def create(self, user: User):
         self.db.add(user)
